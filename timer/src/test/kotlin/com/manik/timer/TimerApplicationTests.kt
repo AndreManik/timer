ccd53d52748b0c2c28bb9f9d47e37aa3bad60f67
Andrei Manik <andre.ivan.manik@gmail.com>
2023-05-14 15:38:05 +0300

    [#1] init project | Close #1

---
+++ timer/src/test/kotlin/com/manik/timer/TimerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.manik.timer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
